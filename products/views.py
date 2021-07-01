17ab81cc5d326d3c9ff346f8367afb6f75ef2509
Hovo-93 <ovo.aroyan@mail.ru>
2021-07-01 14:11:10 +0400

    'lesson2'

--- products/views.py
+++ products/views.py
@@ -3,7 +3,30 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'products/index.html')
+    context = {
+        'title': 'GeekShop Store',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals'},
+            {'name': 'Синяя куртка The North Face'},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN'},
+        ]
+    }
+    return render(request, 'products/index.html', context)
+
 
 def products(request):
-    return render(request,'products/products.html')
+    context = {
+        'title': 'GeekShop Products',
+        'products': [
+            {'name': 'Худи черного цвета с монограммами adidas Originals'},
+            {'name': 'Синяя куртка The North Face'},
+            {'name': 'Коричневый спортивный oversized-топ ASOS DESIGN'},
+        ],
+        'promotion': True,
+        'promotion_of_products': [
+            {'name': 'Черный рюкзак Nike Heritage'},
+            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex'},
+            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN'},
+        ],
+    }
+    return render(request, 'products/products.html', context)
