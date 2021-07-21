a68b3b0221705db701b1c9799386b432e7e0b2ac
Hovo-93 <ovo.aroyan@mail.ru>
2021-07-21 13:36:33 +0400

    'lesson8'

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from products.models import Product,ProductCategory
+from products.models import Product, ProductCategory
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger
 import json
 import os
 
@@ -19,21 +20,35 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-    context = {
-        'title': 'GeekShop Products',
-        'products':Product.objects.all(),
-        'categories':ProductCategory.objects.all(),
-        'promotion': True,
-        'promotion_of_products': [
-            {'name': 'Черный рюкзак Nike Heritage',
-             'price': 2340},
-            {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
-             'price': 13590},
-            {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
-             'price': 2890},
-        ],
-    }
-
-
+# def products(request):
+#     context = {
+#         'title': 'GeekShop Products',
+#         'products': Product.objects.all(),
+#         'categories': ProductCategory.objects.all(),
+#         'promotion': True,
+#         'promotion_of_products': [
+#             {'name': 'Черный рюкзак Nike Heritage',
+#              'price': 2340},
+#             {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex',
+#              'price': 13590},
+#             {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN',
+#              'price': 2890},
+#         ],
+#     }
+#
+#     return render(request, 'products/products.html', context)
+def products(request,category_id=None,page=1):
+    context = {'title': 'GeekShop Products','categories': ProductCategory.objects.all()}
+    if category_id:
+        products = Product.objects.filter(category_id= category_id)
+    else:
+        products = Product.objects.all()
+    paginator =Paginator(products,3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
+    context['products'] = products_paginator
     return render(request, 'products/products.html', context)
